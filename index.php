9f42106e24ca70081d6e212fb2d9610fde043898
Noob-Ovi <abdullah.ovi098@gmail.com>
2024-09-14 13:56:17 +0600

    Learnt arithmetic operation

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
-echo"Hello new world <br>";
-echo"Hello world";
-$name = "Abdullah ";
-$age = 40;
-echo "my name is {$name} and age is {$age}"
+
+
+// Variable and datatype:
+// $name = "Abdullah";
+// $age = 40;
+// $balance = 3000.21;
+// $married = false;
+// $single = true;
+// $remaining = null;
+// $remaining = $balance - 20;
+// echo "my name is {$name} and age is {$age} and bank balance is \${$balance} <br>"; // \ to show $ sign
+// echo"Married: {$married} <br>"; //false shows nothing
+// echo"Single: {$single}"; //true shows 1
+// echo "remaining balance: \${$remaining}"
+
+// Arithmetic operator:
+$x = 10;
+$y = 4;
+$z = null;
+$counter=  0;
+
+// $z = $x / $y; 
+// $z = $x ** $y; // To do power 
+// $z = pow($x,$y);
+// echo $x + $y;
+// echo $z;
+$counter++;
+$counter += 1;
+echo $counter;
+
+
 ?>

7d03b57223c9f8f44abcf2e3053910dbd4a0aeea
Noob-Ovi <abdullah.ovi098@gmail.com>
2024-09-17 01:35:15 +0600

    learnt function

---
+++ func.php
@@ -0,0 +1,32 @@
+<?php
+// function is_even($num){
+//     if($num<=0){
+//         return "Provide valid number";
+//     }
+// else if($num %2 == 0){
+//     return "Even";
+// }
+// else{
+//     return "Odd";
+// }
+// }
+
+// $res = is_even(1);
+// echo $res;
+
+// function do_sum($nums){
+//     if(is_array($nums) && count($nums)>0){
+//         $sum=0;
+//         for($i=0;$i<=count($nums)-1;$i++){
+//             $sum = $sum + $nums[$i];
+//         }
+//         return $sum;
+//     }
+//     else{
+//         echo "Provide array of numbers";
+//     }
+
+// }
+
+// $res= do_sum([67,8]);
+// echo $res;
